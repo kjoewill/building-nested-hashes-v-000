6bb2501db44f822a12973f66a2ebf69304a6b662
Kevin W <kjoewill@gmail.com>
2018-08-22 23:02:14 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,9 +1,4 @@
-def first_challenge
-  epic_tragedy = {
-    montague: {},
-    capulet:  {}
-  }
-  puts epic_tragedy[:montague]
-end
-
-first_challenge
+epic_tragedy = {
+  :montague => {},
+  :capulet => {}
+}
